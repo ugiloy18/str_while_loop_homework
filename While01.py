1f20ab1f69cda9fbcaa0ed5fbe6175907969cd3d
ogiloy <ogiloykurbanyeva@gmail.com>
2025-07-24 13:14:33 +0500

    bajarildi

--- While01.py
+++ While01.py
@@ -1,9 +1,17 @@
 def main(s):
-    """
+    """"
     A variable of type str is given. Find how many digits it contains and return.
     Args:
         s: str
     Returns:
         int: return answer
     """
-    return
+    count = 0
+    i = 0
+    while i < len(s):
+        if s[i].isdigit():
+            count += 1
+        i += 1
+    return count 
+print(main("bab5vt4b9"))
+print(main("kur85n48f85hr4r8hf84"))
