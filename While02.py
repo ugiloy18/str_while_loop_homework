42658bb221d495b1e648aa873732d2e5551d3ce3
ogiloy <ogiloykurbanyeva@gmail.com>
2025-07-24 13:14:50 +0500

    bajarildi

--- While02.py
+++ While02.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    count = 0
+    i = 0
+    while i < len(s):
+        if s[i].isalpha():
+            count += 1
+        i += 1
+    return count
+print(main("avtomatlashtirilmaganligida"))
